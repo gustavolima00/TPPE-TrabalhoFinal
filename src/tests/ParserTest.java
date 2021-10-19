d01dc7e6ac6907433888e2b5268bc60c1baa3e7b
Gustavo Lima <gustavomarques.gml@gmail.com>
2021-10-19 19:09:07 -0300

    Addition of files structure

---
+++ src/tests/ParserTest.java
@@ -0,0 +1,12 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ParserTest {
+	@Test
+	public void Test1(){
+		assertEquals(1, 1);
+	}
+}
