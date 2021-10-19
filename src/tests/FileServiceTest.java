00a0527ab53398f0b19910bbd50dd407b9c40aea
Gustavo Lima <gustavomarques.gml@gmail.com>
2021-10-19 20:51:28 -0300

    Addition of CanThrowException1

--- src/tests/FileServiceTest.java
+++ src/tests/FileServiceTest.java
@@ -1,23 +1,39 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.io.IOException;
 
 import org.junit.Test;
 
+import exceptions.ArquivoNaoEncontradoException;
 import services.FileService;
 
-class MockData {
+class CorrectMockData {
 	static String FILE_PATH = "src/files/hello_world.txt";
 	static String FILE_CONTENT = "hello_world";
 }
 
+class WrongMockData {
+	static String FILE_PATH = "wrong/file/path.txt";
+	static String EXPECTED_MESSAGE = "O arquivo" + WrongMockData.FILE_PATH + " nao foi encontrado";
+}
+
 public class FileServiceTest {
 	@Test
-	public void CanReadFile1() throws IOException{
-		String result = FileService.ReadFileAsString(MockData.FILE_PATH);
-		assertEquals(result, MockData.FILE_CONTENT);
+	public void CanReadFile1() throws IOException, ArquivoNaoEncontradoException{
+		String result = FileService.ReadFileAsString(CorrectMockData.FILE_PATH);
+		assertEquals(result, CorrectMockData.FILE_CONTENT);
+	}
+	
+	@Test
+	public void CanThrowException1() throws IOException, ArquivoNaoEncontradoException{
+		ArquivoNaoEncontradoException thrown = assertThrows(
+				ArquivoNaoEncontradoException.class,
+		           () -> {FileService.ReadFileAsString(WrongMockData.FILE_PATH);}
+		    );
+		assertEquals(thrown.getMessage(), WrongMockData.EXPECTED_MESSAGE);
 	}
 	
 }
